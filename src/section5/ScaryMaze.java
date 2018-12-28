fa3be92b75d29b53dbf2950f60993b0740095b90
DISCORDDARRELL <league@WorthyMini-5.attlocal.net>
2018-12-28 13:13:19 -0800

    day three
    last daY

--- src/section5/ScaryMaze.java
+++ src/section5/ScaryMaze.java
@@ -16,23 +16,27 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
-public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener {
+public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener {public static void main1(String[] args) {
+	
+}
+	
+
 	
 	BufferedImage maze;
-	final int frameWidth = 800;
-	final int frameHeight = 600;
+	final int frameWidth = 1000;
+	final int frameHeight = 1000;
 
 	ScaryMaze() throws Exception {
 		//1. Use this online tool to make a maze image and drop it into your section5 package: http://pixlr.com/editor/
-		//maze = ImageIO.read(getClass().getResource("standardMaze.jpg"));
+		maze = ImageIO.read(getClass().getResource("pixil-frame-0.png"));
 		//2. Change the line of code above so that it matches your maze's file name
 		
 		//3. Set the mouse pointer to the start of your maze using:
 		//new Robot().mouseMove(45, 95);
-		
+		new Robot().mouseMove(85, 120);
 		//4. Add a mouse motion listener using:
 		//addMouseMotionListener(this);
-		
+		addMouseMotionListener(this);
 	}
 
 	@Override
@@ -41,17 +45,21 @@ public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener {
 		int mouseY = e.getY();
 		int mouseColor = maze.getRGB(mouseX, mouseY);
 		//5. Print the mouseColor variable 
-		
+		System.out.println(mouseColor);
 		//6.  Run your program and put your mouse over the background to find out what color it is
 		
 		//7. Create an int variable that holds the background color. 
-		
+		int backgroundColor = -12441168;
 		//8. If the mouse falls off the path (if it is on the background)
-	
+		if (mouseColor == backgroundColor) {
+			scare();
+		}
 				// call the scare method
 		
 		//13. If the mouse is on the end color
-		
+		if(mouseColor ==  -16738680) {
+			JOptionPane.showMessageDialog(null,("u win good job"));
+		}
 				// pop up a message to tell them they won
 		
 	}
